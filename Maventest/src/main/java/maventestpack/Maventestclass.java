f1b1ee1097c904fb3000dea82f72cdcc9afcfa1e
HP <HP@LAPTOP-OP0RTV2C>
2024-03-26 19:32:06 +0530

    Maven test commit

---
+++ Maventest/src/main/java/maventestpack/Maventestclass.java
@@ -0,0 +1,11 @@
+package maventestpack;
+
+public class Maventestclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Maventest");
+
+	}
+
+}
